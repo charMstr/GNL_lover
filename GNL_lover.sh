9fdc337d8f6f2b7167dbfac57de3a39554323572
Charlie ARMSTRONG <charmstr@e1r4p3.42.fr>
2019-11-21 04:44:34 +0100

    modified the script so that it checks if the read functions is called with the BUFFER_SIZE arg

--- GNL_lover.sh
+++ GNL_lover.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-WEIRD_BG="\033[48;5;203m"
+WEIRD_BG="\033[48;5;194m"
 BLACK_FG="\033[38;5;0m"
 BLACK_BG="\033[48;5;0m"
 
@@ -46,7 +46,7 @@ do
 		fi
 	elif [ ${MAIN_NAME} == "mains/main_no_end_of_line_at_end.c" ] 							########						#modify the iF in a elif
 	then
-		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t\t${MAIN_NAME}\t\t\t${CLEAR_COLOR}"
+		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t${MAIN_NAME}\t${CLEAR_COLOR}"
 		for TEST in `seq 10 17`
 		do
 			TEST_FILE=./test_files_GNL/test_file${TEST}
@@ -67,10 +67,10 @@ do
 		done
 	elif [ ${MAIN_NAME} == "mains/main_INPUTS_WRONG.c" ]
 	then
-		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t\t${MAIN_NAME}\t\t\t${CLEAR_COLOR}"
+		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t${MAIN_NAME}\t\t${CLEAR_COLOR}"
 		SIZE=3
 		TEST_FILE=Makefile
-		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t\t${TEST_FILE}\t\t${CLEAR_COLOR} BUFFER_SIZE=${SIZE}"
+		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t\t${TEST_FILE}\t\t${CLEAR_COLOR}"
 		make re MAIN=${MAIN_NAME} BUF_SIZE=${SIZE}	
 		./get_next_line ${TEST_FILE}
 		RESULT=$?
@@ -82,10 +82,10 @@ do
 		fi
 	elif [ ${MAIN_NAME} == "mains/main_dev_null.c" ];
 	then
-		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t\t${MAIN_NAME}\t\t\t${CLEAR_COLOR}"
+		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t${MAIN_NAME}\t\t\t${CLEAR_COLOR}"
 		SIZE=12
 		TEST_FILE=/dev/null
-		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t\t${WEIRD_BG}${BLACK_FG}${TEST_FILE}${TEST_FILE_BG}${BLACK_FG}\t\t${CLEAR_COLOR} BUFFER_SIZE=${SIZE}"
+		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t${WEIRD_BG}${BLACK_FG}\t${TEST_FILE}\t${TEST_FILE_BG}${BLACK_FG}\t${CLEAR_COLOR}"
 		make re MAIN=${MAIN_NAME} BUF_SIZE=${SIZE}	
 		./get_next_line ${TEST_FILE}
 		RESULT=$?
@@ -97,14 +97,23 @@ do
 		fi
 	elif [ ${MAIN_NAME} == "mains/main_STDIN_FILENO.c" ];
 	then
-		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t\t${MAIN_NAME}\t\t\t${CLEAR_COLOR}"
+		echo "\n\n\tbuilding with main: ${MAIN_BG}${BLACK_FG}\t\t${MAIN_NAME}\t\t${CLEAR_COLOR}"
 		SIZE=12
 		TEST_FILE=/dev/stdin
-		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t\t${WEIRD_BG}${BLACK_FG}${TEST_FILE}${TEST_FILE_BG}${BLACK_FG}\t\t${CLEAR_COLOR} BUFFER_SIZE=${SIZE}"
+		echo "\n\n\t\ttest_file is:\t\t${TEST_FILE_BG}${BLACK_FG}\t${WEIRD_BG}${BLACK_FG}\t${TEST_FILE}\t${TEST_FILE_BG}${BLACK_FG}\t${CLEAR_COLOR} BUFFER_SIZE=${SIZE}"
 		make re MAIN=${MAIN_NAME} BUF_SIZE=${SIZE}	
 		./get_next_line
 	fi
 done
+echo "\n\n\tcheck if the read function is called with the BUFFER_SIZE defined argument:\n"
+cat copy_in_here_GNL_files/get_next_line.c copy_in_here_GNL_files/get_next_line_utils.c | grep "read(" | grep -n --colour "BUFFER_SIZE"
+RESULT=$?
+if [ ${RESULT} == 0 ]
+then
+	echo "\t=====> ✅\n"
+else
+	echo "\t=====> ❌\n"
+fi
 
 rm user_output
 make fclean
